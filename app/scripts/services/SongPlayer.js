381f59d8b9fac9c15526dfdaac4996d13471125a
Courtney Barley <crbarley@gmail.com>
2017-05-17 00:56:00 -0700

    Assignment 7 Services Part 2: Complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -28,14 +28,22 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays the current Buzz object(song) and sets property of song object to true
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
     
         
     SongPlayer.play = function(song) {
         if(currentSong !== song) {
  
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
             
         } else if (currentSong === song) {
             if (currentBuzzObject.isPaused()) {
